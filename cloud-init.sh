6c683d3b37077cd3829ea282bd9c1d1f025d9d9b
scott coulton <scott.coulton@microsoft.com>
2021-09-23 10:02:30 +1000

    fixing cloudinit.sh

--- cloud-init.sh
+++ cloud-init.sh
@@ -6,27 +6,46 @@ export DEBIAN_FRONTEND=noninteractive
 echo "# make..."
 sudo apt update 
 sudo apt install -y \
-         make \
-         uidmap
+         git \
+         nodejs \
+         npm \
+
         
-echo "# path..."
-sudo tee -a /etc/profile.d/nerdctl.sh <<'EOF'
-export PATH="$PATH:/usr/local/nerdctl/bin"
-EOF
-sudo chmod a+x /etc/profile.d/nerdctl.sh
+echo "# rust..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup target add wasm32-wasi
 
-echo "# nerdctl..."
-curl -OL https://github.com/containerd/nerdctl/releases/download/v0.11.1/nerdctl-full-0.11.1-linux-amd64.tar.gz
-sudo mkdir -p /usr/local/nerdctl
-sudo tar -C /usr/local/nerdctl -xzf nerdctl-full-0.11.1-linux-amd64.tar.gz
-sudo mkdir -p /opt/cni/bin/
-sudo ln -s /usr/local/nerdctl/libexec/cni/* /opt/cni/bin/
+echo "# dotnet5..."
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y aspnetcore-runtime-5.0
 
-echo "# rootless ..."
-sudo chown ubuntu:ubuntu /usr/local/nerdctl/bin/containerd-rootless-setuptool.sh
-sudo mv /usr/local/nerdctl/bin/containerd-rootless-setuptool.sh .
-sudo -E su ubuntu -c "/usr/bin/bash -l  ./containerd-rootless-setuptool.sh install"
-sudo -E su ubuntu -c "/usr/bin/bash -l  ./containerd-rootless-setuptool.sh install-buildkit"
-sudo -E su ubuntu -c "/usr/bin/bash -l  ./containerd-rootless-setuptool.sh install-stargz"
+echo "# wagi..."
+VERSION=v0.4.0
+wget https://github.com/deislabs/wagi/releases/download/$VERSION/wagi-$VERSION-linux-amd64.tar.gz
+sudo tar -C /usr/local/bin/ -xzf wagi-$VERSION-linux-amd64.tar.gz
+
+echo "bindle..."
+wget https://bindle.blob.core.windows.net/releases/bindle-v0.6.0-linux-amd64.tar.gz
+sudo tar -C /usr/local/bin/ -xzf bindle-v0.6.0-linux-amd64.tar.gz
+bindle-server --unauthenticated
+
+echo "# hipo..."
+git clone https://github.com/deislabs/hippo.git
+cd hippo/Hippo
+dotnet restore
+npm run build
+export BINDLE_URL=http://localhost:8080/v1
+dotnet run
+wget https://github.com/deislabs/hippo-cli/releases/download/v0.9.0/hippo-v0.9.0-linux-amd64.tar.gz
+sudo tar -C /usr/local/bin/ -xzf hippo-v0.9.0-linux-amd64.tar.gz
+
+echo "# yo-wasm..."
+npm install -g yo
+npm install -g generator-wasm
 
 echo "# complete!"
