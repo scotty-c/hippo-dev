e591d8d054b1f26656a8d3ceb214fcbca5894372
scott coulton <scott.coulton@microsoft.com>
2021-09-23 12:04:38 +1000

    fix for user permission on bindle

--- cloud-init.sh
+++ cloud-init.sh
@@ -34,7 +34,7 @@ echo "bindle..."
 wget https://bindle.blob.core.windows.net/releases/bindle-v0.6.0-linux-amd64.tar.gz
 sudo tar -C /usr/local/bin/ -xzf bindle-v0.6.0-linux-amd64.tar.gz
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.config/bindle
-sudo -E su ubuntu -c "/usr/bin/bash -l bindle-server --unauthenticated"
+bindle-server --unauthenticated &
 
 echo "# hipo..."
 git clone https://github.com/deislabs/hippo.git
